57b957cf2c114af883ac8a79e6e533182cd0c9a2
GhibliField <bawyshen@gmail.com>
2019-05-06 16:56:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
